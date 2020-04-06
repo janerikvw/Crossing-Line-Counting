994144ddecfe04a3dbb0326bf7fa2bbcd77cab04
Jan Erik van Woerden <janerikvw2006@gmail.com>
2020-04-06 09:52:13 -0400

    Updated

--- code/CSRNet-pytorch/test.py
+++ code/CSRNet-pytorch/test.py
@@ -10,7 +10,6 @@ import scipy
 import json
 
 import torch
-print(torch.__version__)
 
 import torchvision.transforms.functional as F
 from matplotlib import cm as CM
@@ -24,15 +23,14 @@ transform=transforms.Compose([
                    ])
 
 
-root = '/home/jvwoerden/Thesis/CSRNet-pytorch'
+root = '/home/jvwoerden/Thesis/code/CSRNet-pytorch'
 
 #now generate the ShanghaiA's ground truth
 part_A_train = os.path.join(root,'part_A_final/train_data','images')
 part_A_test = os.path.join(root,'part_A_final/test_data','images')
 part_B_train = os.path.join(root,'part_B_final/train_data','images')
 part_B_test = os.path.join(root,'part_B_final/test_data','images')
-path_sets = [part_A_test]
-
+path_sets = [part_A_train]
 
 img_paths = []
 for path in path_sets:
@@ -41,3 +39,24 @@ for path in path_sets:
 
 
 model = CSRNet()
+
+model = model.cuda()
+checkpoint = torch.load('PartAmodel_best.pth.tar')
+model.load_state_dict(checkpoint['state_dict'])
+
+mae = 0
+for i in xrange(len(img_paths)):
+    img = 255.0 * F.to_tensor(Image.open(img_paths[i]).convert('RGB'))
+
+    img[0,:,:]=img[0,:,:]-92.8207477031
+    img[1,:,:]=img[1,:,:]-95.2757037428
+    img[2,:,:]=img[2,:,:]-104.877445883
+    img = img.cuda()
+    #img = transform(Image.open(img_paths[i]).convert('RGB')).cuda()
+    gt_file = h5py.File(img_paths[i].replace('.jpg','.h5').replace('images','ground_truth'),'r')
+    groundtruth = np.asarray(gt_file['density'])
+    output = model(img.unsqueeze(0))
+    i_mae = abs(output.detach().cpu().sum().numpy()-np.sum(groundtruth))
+    mae += i_mae
+    print i,mae,  i_mae, np.sum(groundtruth), output.detach().cpu().sum().numpy()
+print mae/len(img_paths)
