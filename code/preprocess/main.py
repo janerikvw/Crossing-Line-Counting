84a5e3d6b3d46c36776989a9eac435be29a8939c
Jan Erik van Woerden <janerikvw2006@gmail.com>
2020-04-07 10:05:24 -0400

    Added fudan preprocess and make entitity objects

--- code/preprocess/main.py
+++ code/preprocess/main.py
@@ -8,64 +8,21 @@ import scipy.spatial
 import glob
 from matplotlib import pyplot as plt
 import json
-import ucsdpeds
 from PIL import ImageDraw, Image
+from glob import glob
 
 import matplotlib
 
 from tqdm import tqdm
 
-#partly borrowed from https://github.com/davideverona/deep-crowd-counting_crowdnet
-def gaussian_filter_density(img,points):
-    '''
-    This code use k-nearst, will take one minute or more to generate a density-map with one thousand people.
-
-    points: a two-dimension list of pedestrians' annotation with the order [[col,row],[col,row],...].
-    img_shape: the shape of the image, same as the shape of required density-map. (row,col). Note that can not have channel.
-
-    return:
-    density: the density-map we want. Same shape as input image but only has one channel.
-
-    example:
-    points: three pedestrians with annotation:[[163,53],[175,64],[189,74]].
-    img_shape: (768,1024) 768 is row and 1024 is column.
-    '''
-    img_shape=[img.shape[0],img.shape[1]]
-    density = np.zeros(img_shape, dtype=np.float32)
-    gt_count = len(points)
-    if gt_count == 0:
-        return density
-
-    leafsize = 2048
-    # build kdtree
-    tree = scipy.spatial.KDTree(points.copy(), leafsize=leafsize)
-
-    # query kdtree
-    distances, locations = tree.query(points, k=4)
-
-    for i, pt in enumerate(points):
-        pt2d = np.zeros(img_shape, dtype=np.float32)
-        if int(pt[1])<img_shape[0] and int(pt[0])<img_shape[1]:
-            pt2d[int(pt[1]),int(pt[0])] = 1.
-        else:
-            continue
-        if gt_count > 1:
-            sigma = (distances[i][1]+distances[i][2]+distances[i][3])*0.1
-        else:
-            sigma = np.average(np.array(gt.shape))/2./2.
-        density += gaussian_filter(pt2d, sigma, mode='constant', truncate=3.0)
-
-    return density
-
-def handle_frame(frame_info):
-    img = plt.imread(frame_info['filename'])
-    k = gaussian_filter_density(img, np.array(frame_info['persons']))
-    np.save(frame_info['filename'].replace('.png', '.npy'), k)
+from density_filter import gaussian_filter_density
 
 
+def handle_frame(frame_obj):
+    k = gaussian_filter_density(frame_obj)
+    np.save(frame_obj.get_image_path().replace('.png', '.npy'), k)
 
 class Consumer(multiprocessing.Process):
-
     def __init__(self, task_queue):
         multiprocessing.Process.__init__(self)
         self.task_queue = task_queue
@@ -76,7 +33,7 @@ class Consumer(multiprocessing.Process):
             next_task = self.task_queue.get()
             if next_task is None:
                 # Poison pill means shutdown
-                #print('%s: Exiting' % proc_name)
+                print('%s: Exiting' % proc_name)
                 self.task_queue.task_done()
                 break
 
@@ -97,6 +54,12 @@ class Task(object):
 
 
 if __name__ == '__main__':
+    import fudan
+
+    # Loading all the videos based on your dataset in BasicVideo/BasicFrame object format
+    videos = []
+    for video_path in glob('../data/Fudan/*/*'):
+        videos.append(fudan.load_video(video_path))
 
     # Establish communication queues
     tasks = multiprocessing.JoinableQueue()
@@ -108,13 +71,10 @@ if __name__ == '__main__':
     for w in consumers:
         w.start()
 
-    all_frame_files = ucsdpeds.get_all_frame_files('../data/ucsdpeds/gt/vidf/', '../data/ucsdpeds/vidf/')
     # Put every frame in the queue
-    for movie_path in all_frame_files:
-        all_frame_info = ucsdpeds.get_video_info(movie_path[0], movie_path[1])
-
-        for frame_info in all_frame_info:
-            tasks.put(Task(frame_info))
+    for video in videos:
+        for frame_obj in video.get_frames():
+            tasks.put(Task(frame_obj))
 
     # Add a poison pill for each consumer
     for i in range(num_consumers):
@@ -128,8 +88,6 @@ if __name__ == '__main__':
         pbar.update(diff)
         last_queue = tasks.qsize()
         time.sleep(0.2)
-
-
 
     # Wait for all of the tasks to finish
     tasks.join()
