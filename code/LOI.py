f39f8aff36eba604c47990a554c42e0fe9316297
Jan Erik van Woerden <janerikvw2006@gmail.com>
2020-04-17 09:01:30 -0400

    LOI update

--- code/LOI.py
+++ code/LOI.py
@@ -1,13 +1,17 @@
 import numpy as np
 import torch
+import math
 
 import datasets
 import LOI
 
+import scipy.misc
+from scipy.ndimage import rotate
+
 
 from CSRNet.model import CSRNet
 import torchvision.transforms.functional as F
-import PIL.Image as Image
+from PIL import Image, ImageDraw
 
 import tensorflow as tf
 from DDFlow.network import pyramid_processing
@@ -16,9 +20,121 @@ from DDFlow.flowlib import flow_to_color
 def pixelwise_loi(counting_result, flow_result):
     return
 
+def region_to_mask(region, img_width=1920, img_height=1080):
+    p1 = region[0]
+    p2 = region[2]
+    mask = np.zeros((img_width, img_height))
+    mask[min(p1[0], p2[0]):max(p1[0], p2[0]), min(p1[1], p2[1]):max(p1[1], p2[1])] = 1
+    return mask
 
-def regionwise_loi(counting_result, flow_result):
-    return
+def init_regionwise_loi(point1, point2, img_width=1920, img_height=1080, width=50, cregions=5, shrink=0.90):
+    center = (img_width / 2, img_height / 2)
+    rotate_angle = math.degrees(math.atan((point2[1] - point1[1]) / float(point2[0] - point1[0])))
+
+    regions = select_regions(rotate_point(point1, -rotate_angle, center), rotate_point(point2, -rotate_angle, center),
+                             width=width, regions=cregions, shrink=shrink)
+
+    return regions, rotate_angle, center
+
+def regionwise_loi(counting_result, flow_result, loi_info):
+    regions, rotate_angle, center = loi_info
+
+    sums = ([], [])
+
+    img = counting_result
+    img = rotate(img, rotate_angle, reshape=False)
+
+    for i, small_regions in enumerate(regions):
+        for o, region in enumerate(small_regions):
+            mask = region_to_mask(region)
+            part = mask.transpose() * img
+            sums[i].append(part.sum() / 1000)
+
+    return sums
+
+
+def rotate_point(point, angle, center):
+    r_angle = math.radians(angle)
+    r00 = math.cos(r_angle)
+    r01 = -math.sin(r_angle)
+    r10 = math.sin(r_angle)
+    r11 = math.cos(r_angle)
+
+    out = (
+        r00 * point[0] + r01 * point[1] + center[0] - r00 * center[0] - r01 * center[1],
+        r10 * point[0] + r11 * point[1] + center[1] - r10 * center[0] - r11 * center[1]
+    )
+
+    return out
+
+
+def select_regions(dot1, dot2, width=50, regions=5, shrink=0.90):
+    region_lines = []
+    line_points = np.linspace(np.array(dot1), np.array(dot2), num=regions+1).astype(int)
+    for i, point in enumerate(line_points):
+        if i + 1 >= len(line_points):
+            break
+
+        point2 = line_points[i + 1]
+        region_lines.append((tuple(list(point)),  tuple(list(point2))))
+
+    regions = ([], [])
+
+    region_lines.reverse()
+
+    for point1, point2 in region_lines:
+        part_line_length = math.sqrt(math.pow(point1[0] - point2[0], 2) + math.pow(point1[1] - point2[1], 2))
+        point_diff = (
+            - (point1[1] - point2[1]) / float(part_line_length) *  float(width),
+            (point1[0] - point2[0]) / float(part_line_length) * float(width)
+        )
+
+        width *= shrink
+
+        regions[0].append([
+            point1,
+            point2,
+            (int(point2[0] + point_diff[0]), int(point2[1] + point_diff[1])),
+            (int(point1[0] + point_diff[0]), int(point1[1] + point_diff[1]))
+        ])
+
+        regions[1].append([
+            point1,
+            point2,
+            (int(point2[0] - point_diff[0]), int(point2[1] - point_diff[1])),
+            (int(point1[0] - point_diff[0]), int(point1[1] - point_diff[1]))
+        ])
+
+    regions[0].reverse()
+    regions[1].reverse()
+
+    return regions
+
+
+def image_add_region_lines(image, dot1, dot2, loi_output=None, width=50, nregions=5, shrink=0.90):
+    regions = select_regions(dot1, dot2, width=width, regions=nregions, shrink=shrink)
+    draw = ImageDraw.Draw(image)
+
+    for i, small_regions in enumerate(regions):
+        for o, region in enumerate(small_regions):
+
+            if i == 0:
+                outline_c = 100
+            else:
+                outline_c = 300
+
+            draw.polygon(region, outline=outline_c)
+
+            if loi_output:
+                msg = str(loi_output[i][o])
+                w, h = draw.textsize(msg)
+                center = (
+                    (region[0][0] + region[2][0] - w) / 2,
+                    (region[0][1] + region[2][1] - h) / 2
+                )
+                draw.text(center, msg, fill="white")
+
+    draw.line((dot1[0], dot1[1], dot2[0], dot2[1]), fill=200, width=10)
 
 
 # Initialize the crowd counting model
@@ -44,7 +160,9 @@ def run_cc_model(cc_model, frame):
     img[2, :, :] = img[2, :, :] - 104.877445883
     img = img.cuda()
     cc_output = cc_model(img.unsqueeze(0))
-    return cc_output.detach().cpu().data.numpy().squeeze()
+    cc_output = cc_output.detach().cpu().data.numpy().squeeze()
+
+    return scipy.misc.imresize(cc_output, size=8.0)
 
 
 # Initialize the flow estimation model
--- code/main.py
+++ code/main.py
@@ -2,45 +2,56 @@ import torch
 import sys
 
 import datasets.factory as factory
+import datasets.fudan as fudan
 import LOI
 
 from scipy import misc, io
+from scipy.ndimage import rotate
+import numpy as np
 
-import PIL.Image as Image
+from PIL import Image, ImageDraw
 
 print("--- LOADING TESTSET ---")
-train_pairs, test_pairs = factory.load_train_test_frame_pairs('')
-print("- Loaded {} pairs".format(len(test_pairs)))
+#train_pairs, test_pairs = factory.load_train_test_frame_pairs('')
+
+train_pairs = fudan.load_video('data/Fudan/train_data/72').get_frame_pairs()
+print("- Loaded {} pairs".format(len(train_pairs)))
 print("--- DONE ---")
 
 # Load counting model
 cc_model = LOI.init_cc_model()
 
 # Load flow estimation model
-fe_model = LOI.init_fe_model()
+#fe_model = LOI.init_fe_model()
 
-for i,pair in enumerate(train_pairs[200:400]):
+point1 = (320, 400)
+point2 = (1250, 460)
+loi_region_info = LOI.init_regionwise_loi(point1, point2)
 
-    print("{}/200".format(i))
+for i,pair in enumerate(train_pairs):
+
+    print("{}/{}".format(i, len(train_pairs)))
     # Run counting model on frame A
     cc_output = LOI.run_cc_model(cc_model, pair.get_frames()[0])
 
     # Run flow estimation model on frame pair
-    fe_output = LOI.run_fe_model(fe_model, pair)
+    #fe_output = LOI.run_fe_model(fe_model, pair)
 
     # Run the merger for the crowdcounting and flow estimation model
     # In code mention the papers based on
-    #loi_output = LOI.pixelwise_loi(cc_output, fe_output)
-    loi_output = LOI.regionwise_loi(cc_output, fe_output)
-
-    # print(cc_output.shape)
+    fe_output = None
+    loi_output = LOI.regionwise_loi(cc_output, fe_output, loi_region_info)
     #
     # img = Image.fromarray(cc_output * 255.0 / cc_output.max())
     # img = img.convert("L")
     # img.save('results/csr.png')
-    #
-    # img = Image.open(pair.get_frames()[0].get_image_path())
-    # img = img.convert("L")
-    # img.save('results/original.png')
-    #
+
+    img = pair.get_frames()[0].get_image().convert("RGB")
+
+    LOI.image_add_region_lines(img, point1, point2, loi_output)
+
+    img = img.crop((point1[0]-50, point1[1]-200, point2[0]+50, point2[1]+200))
+
+    img.save('results/video/r_{}.png'.format(i))
+
     # misc.imsave('results/flow_est_.png', fe_output[1][0])
